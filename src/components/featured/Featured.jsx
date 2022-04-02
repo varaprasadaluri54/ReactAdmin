bbed5e5d5ebcef94545f7fc4e008300bbd3eddcb
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-04-02 15:25:54 -0400

    src

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,14 +1,55 @@
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import "./featured.scss";
-
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 export default function Featured() {
   return (
     <div className="featured">
       <div className="top">
         <h1 className="title">Total Revenue</h1>
-        <MoreVertIcon fontsize="small" />
+        <MoreVertIcon fontSize="small" />
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={4} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">
+          Previous test test test test test test test test test test test test
+          test test test test test test
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+        </div>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <KeyboardArrowDownIcon fontSize="small" />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
